fb27623a15ca282edaec914de3439060e7e21055
Rémi DEHER <remi.deher@deher.net>
2025-08-12 02:48:31 +0200

    refonte de la page de connexion

--- templates/login.php
+++ templates/login.php
@@ -1,25 +1,39 @@
 <?php
 // templates/login.php
-$body_class = 'login-page-body';
-$page_css = 'admin.css'; // La page de login utilise les styles admin
-require __DIR__ . '/partials/header.php';
+$body_class = 'login-page-body'; // Classe pour le body
+require 'partials/header.php';
 ?>
-<div class="login-container">
-    <h2>Administration</h2>
-    <?php if (isset($error)): ?>
-        <p class="error"><?= htmlspecialchars($error) ?></p>
-    <?php endif; ?>
-    <form action="index.php?page=login" method="POST">
-        <div class="form-group">
-            <label for="username">Nom d'utilisateur</label>
-            <input type="text" id="username" name="username" required>
+<div class="login-wrapper">
+    <div class="login-container">
+        <!-- NOUVEAU BOUTON DE RETOUR -->
+        <a href="index.php?page=calculateur" class="return-button" title="Retour au calculateur">
+            <i class="fa-solid fa-times"></i>
+        </a>
+
+        <div class="login-header">
+            <h2>&#128176; Comptage Caisse</h2>
+            <p>Connectez-vous à votre espace d'administration</p>
         </div>
-        <div class="form-group">
-            <label for="password">Mot de passe</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button type="submit" class="save-btn">Connexion</button>
-    </form>
+
+        <?php if (isset($error)): ?>
+            <p class="error"><?= htmlspecialchars($error) ?></p>
+        <?php endif; ?>
+
+        <form action="index.php?page=login" method="POST">
+            <div class="form-group with-icon">
+                <i class="fa-solid fa-user input-icon"></i>
+                <input type="text" id="username" name="username" placeholder="Nom d'utilisateur" required autofocus>
+            </div>
+            <div class="form-group with-icon">
+                <i class="fa-solid fa-lock input-icon"></i>
+                <input type="password" id="password" name="password" placeholder="Mot de passe" required>
+            </div>
+            <button type="submit" class="save-btn" style="width: 100%;">Se connecter</button>
+        </form>
+    </div>
 </div>
+<?php
+// Pas de footer sur la page de connexion
+?>
 </body>
 </html>
