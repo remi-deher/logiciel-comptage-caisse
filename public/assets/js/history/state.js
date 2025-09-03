b8efad37b12d3a8c39c5f9ce3a67b744c757b116
Rémi DEHER <remi.deher@deher.net>
2025-09-03 09:13:27 +0200

    Suppression fichier inutile

--- public/assets/js/history/state.js
+++
@@ -1,9 +0,0 @@
-// public/js/history/state.js
-// Ce module contient l'état partagé et modifiable pour la page d'historique.
-
-export const state = {
-    globalChart: null,
-    modalChart: null,
-    // La sélection pour la comparaison est maintenant gérée ici
-    selectedForComparison: [], 
-};
