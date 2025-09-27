d18b250b792e62fcc45261b7d2485c3a85083f14
Rémi DEHER <remi.deher@deher.net>
2025-09-27 18:40:52 +0200

    correction écart historique

--- src/CalculateurController.php
+++ src/CalculateurController.php
@@ -1,5 +1,5 @@
 <?php
-// Fichier : src/CalculateurController.php (Corrigé pour l'autosave multi-utilisateurs)
+// Fichier : src/CalculateurController.php (Version finale avec toutes les corrections)
 
 require_once __DIR__ . '/services/VersionService.php';
 require_once __DIR__ . '/Utils.php';
@@ -100,8 +100,7 @@ class CalculateurController {
             $this->pdo->beginTransaction();
             $nom_comptage = trim($_POST['nom_comptage'] ?? '');
             if ($is_autosave) {
-                // La ligne supprimant les anciennes sauvegardes a été retirée ici
-                // pour permettre un fonctionnement correct en multi-utilisateurs.
+                // La ligne supprimant les anciennes sauvegardes a été retirée pour un fonctionnement multi-utilisateurs correct.
                 $nom_comptage = "Sauvegarde auto du " . date('Y-m-d H:i:s');
             } else {
                 $nom_comptage = empty($nom_comptage) ? "Comptage du " . date('Y-m-d H:i:s') : $nom_comptage;
@@ -198,7 +197,6 @@ class CalculateurController {
                             if (is_array($releves)) {
                                 foreach ($releves as $releve) {
                                     $stmt_cb = $this->pdo->prepare("INSERT INTO comptage_cb (comptage_detail_id, terminal_id, montant, heure_releve) VALUES (?, ?, ?, ?)");
-                                    // CORRECTION : On nettoie également ici pour être cohérent
                                     $heure_releve_raw = $releve['heure'] ?? null;
                                     $heure_releve = ($heure_releve_raw && $heure_releve_raw !== 'undefined' && $heure_releve_raw !== 'null' && $heure_releve_raw !== '') ? $heure_releve_raw : null;
                                     $stmt_cb->execute([$new_comptage_detail_id, $terminal_id, $releve['montant'], $heure_releve]);
@@ -285,7 +283,30 @@ class CalculateurController {
                 }
             }
             
-            // ... (logique pour TPE et Chèques inchangée)
+            if (isset($caisse_data['tpe']) && is_array($caisse_data['tpe'])) {
+                foreach ($caisse_data['tpe'] as $terminal_id => $releves) {
+                    if (is_array($releves)) {
+                        foreach ($releves as $releve) {
+                            $montant_val = get_numeric_value($releve, 'montant');
+                            if ($montant_val > 0) {
+                                $stmt_cb = $this->pdo->prepare("INSERT INTO comptage_cb (comptage_detail_id, terminal_id, montant, heure_releve) VALUES (?, ?, ?, ?)");
+                                $heure_releve_raw = $releve['heure'] ?? null;
+                                $heure_releve = ($heure_releve_raw && $heure_releve_raw !== 'undefined' && $heure_releve_raw !== 'null' && $heure_releve_raw !== '') ? $heure_releve_raw : null;
+                                $stmt_cb->execute([$comptage_detail_id, $terminal_id, $montant_val, $heure_releve]);
+                            }
+                        }
+                    }
+                }
+            }
+            if (isset($caisse_data['cheques']) && is_array($caisse_data['cheques'])) {
+                foreach ($caisse_data['cheques'] as $cheque) {
+                    $montant_cheque = get_numeric_value($cheque, 'montant');
+                    if ($montant_cheque > 0) {
+                        $stmt_cheque = $this->pdo->prepare("INSERT INTO comptage_cheques (comptage_detail_id, montant, commentaire) VALUES (?, ?, ?)");
+                        $stmt_cheque->execute([$comptage_detail_id, $montant_cheque, $cheque['commentaire'] ?? '']);
+                    }
+                }
+            }
 
             $this->clotureStateService->confirmCaisse($caisse_id_a_cloturer);
             
